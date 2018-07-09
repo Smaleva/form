0797dafaf03959fdaa416818ed6a27f894768a03
esmaleva <smaleva.evgenia@yandex.com>
2018-07-09 22:57:43 +0300

    fix js + scss

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ var linkSign = document.querySelector(".signup"),
     formSign = document.querySelector("#signup"),
     formLog = document.querySelector("#login");
 linkSign.addEventListener("click", function (e) {
-    e.preventDefault(),
+    e.preventDefault();
         formSign.classList.remove("none");
         formLog.classList.add("none");
         linkLog.classList.remove("active");
         linkSign.classList.add("active");
 });
 linkLog.addEventListener("click", function (e) {
-    e.preventDefault(),
+    e.preventDefault();
         formSign.classList.add("none");
         formLog.classList.remove("none");
         linkLog.classList.add("active");
@@ -20,13 +20,13 @@ var buttonCountry = document.querySelector("#country"),
     blockCountry = document.querySelector(".country-box"),
     countryInput = document.getElementsByClassName("country-item");
 buttonCountry.addEventListener("click", function (e) {
-    e.preventDefault(),
+    e.preventDefault();
         blockCountry.classList.remove("none");
         blockCountry.classList.add("flex");
 });
 for (var i = 0; i < countryInput.length; i++) {
     countryInput[i].addEventListener("change", function(e) {
-        e.preventDefault(),
+        e.preventDefault();
         blockCountry.classList.remove("flex");
         blockCountry.classList.add("none");
     });
