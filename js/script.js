f68345a8e7e648d120033d87bf2444f73af65ebf
esmaleva <smaleva.evgenia@yandex.com>
2018-07-10 09:26:41 +0300

    отступы

--- js/script.js
+++ js/script.js
@@ -4,28 +4,28 @@ var linkSign = document.querySelector(".signup"),
     formLog = document.querySelector("#login");
 linkSign.addEventListener("click", function (e) {
     e.preventDefault();
-        formSign.classList.remove("none");
-        formLog.classList.add("none");
-        linkLog.classList.remove("active");
-        linkSign.classList.add("active");
+    formSign.classList.remove("none");
+    formLog.classList.add("none");
+    linkLog.classList.remove("active");
+    linkSign.classList.add("active");
 });
 linkLog.addEventListener("click", function (e) {
     e.preventDefault();
-        formSign.classList.add("none");
-        formLog.classList.remove("none");
-        linkLog.classList.add("active");
-        linkSign.classList.remove("active");
+    formSign.classList.add("none");
+    formLog.classList.remove("none");
+    linkLog.classList.add("active");
+    linkSign.classList.remove("active");
 });
 var buttonCountry = document.querySelector("#country"),
     blockCountry = document.querySelector(".country-box"),
     countryInput = document.getElementsByClassName("country-item");
 buttonCountry.addEventListener("click", function (e) {
     e.preventDefault();
-        blockCountry.classList.remove("none");
-        blockCountry.classList.add("flex");
+    blockCountry.classList.remove("none");
+    blockCountry.classList.add("flex");
 });
 for (var i = 0; i < countryInput.length; i++) {
-    countryInput[i].addEventListener("change", function(e) {
+    countryInput[i].addEventListener("change", function (e) {
         e.preventDefault();
         blockCountry.classList.remove("flex");
         blockCountry.classList.add("none");
